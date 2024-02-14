1cce5fdbe11820800c37b875e124bbe3b88ac31c
Cl1493 <152617155+Cl1493@users.noreply.github.com>
2024-02-14 20:30:14 +0000

    Proposición de proyecto

--- src/app.js
+++ src/app.js
@@ -7,5 +7,38 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  /* const selectorhtml = document.getElementById("excuse");
+  selectorhtml.textContent = GeneradorDeExcusas;
+  document.getElementById("generateExcuseBtn")
+  .addEventListener("click", ()) => {location.reload();
+    */
+
+  let who = ["El perro", "Mi abuela", "La tortuga", "El pájaro", "Mi hijo"];
+  let action = ["se comió", "se hizo pipi", "rompió", "ahogó", "tiró"];
+  let what = ["mis deberes", "las llaves", "el coche", "la casa", "el trabajo"];
+  let when = [
+    "antes de clase",
+    "justo ahora",
+    "cuando terminé",
+    "durante la comida",
+    "mientras estaba rezando"
+  ];
+
+  function GeneradorDeExcusas(who, action, what, when) {
+    let randomWho = Math.floor(Math.random() * who.length);
+    let randomAction = Math.floor(Math.random() * action.length);
+    let randomWhat = Math.floor(Math.random() * what.length);
+    let randomWhen = Math.floor(Math.random() * when.length);
+    return (
+      "A lo mejor no te lo vas a creer, pero..." +
+      randomWho +
+      " " +
+      randomAction +
+      " " +
+      randomWhat +
+      " " +
+      randomWhen
+    );
+  }
+  console.log(GeneradorDeExcusas);
 };
