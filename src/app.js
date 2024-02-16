3090b10df6e12e9739421ee90f75172f0fe44e27
Cl1493 <152617155+Cl1493@users.noreply.github.com>
2024-02-16 13:48:46 +0000

    Boton excusa

--- src/app.js
+++ src/app.js
@@ -1,18 +1,5 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
+let Excusa = "";
 window.onload = function() {
-  //write your code here
-  /* const selectorhtml = document.getElementById("excuse");
-  selectorhtml.textContent = GeneradorDeExcusas;
-  document.getElementById("generateExcuseBtn")
-  .addEventListener("click", ()) => {location.reload();
-    */
-
   let who = ["El perro", "Mi abuela", "La tortuga", "El pájaro", "Mi hijo"];
   let action = ["se comió", "se hizo pipi", "rompió", "ahogó", "tiró"];
   let what = ["mis deberes", "las llaves", "el coche", "la casa", "el trabajo"];
@@ -24,21 +11,26 @@ window.onload = function() {
     "mientras estaba rezando"
   ];
 
-  function GeneradorDeExcusas(who, action, what, when) {
-    let randomWho = Math.floor(Math.random() * who.length);
-    let randomAction = Math.floor(Math.random() * action.length);
-    let randomWhat = Math.floor(Math.random() * what.length);
-    let randomWhen = Math.floor(Math.random() * when.length);
-    return (
-      "A lo mejor no te lo vas a creer, pero..." +
-      randomWho +
-      " " +
-      randomAction +
-      " " +
-      randomWhat +
-      " " +
-      randomWhen
-    );
-  }
-  console.log(GeneradorDeExcusas);
+  let randomWho = Math.floor(Math.random() * who.length);
+  let randomAction = Math.floor(Math.random() * action.length);
+  let randomWhat = Math.floor(Math.random() * what.length);
+  let randomWhen = Math.floor(Math.random() * when.length);
+
+  Excusa =
+    "A lo mejor no te lo vas a creer, pero..." +
+    who[randomWho] +
+    " " +
+    action[randomAction] +
+    " " +
+    what[randomWhat] +
+    " " +
+    when[randomWhen];
+  console.log(Excusa);
 };
+
+const element = document.getElementById("botonExcusa");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("aquiLaExcusa").innerHTML = Excusa;
+}
